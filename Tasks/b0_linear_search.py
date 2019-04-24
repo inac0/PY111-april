d11f8ba0826846bce2ca730d6d82308f9b26b420
Лебедев Алексей Аркадьевич <ALELEBE@avalon.ru>
2019-04-24 21:06:50 +0300

    min search

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -2,6 +2,7 @@
 This module implements some functions based on linear search algo
 """
 
+array = [10, 2, 4, 5, 7, 7, 8, 6, 22]
 
 def min_search(arr) -> int:
 	"""
@@ -10,8 +11,12 @@ def min_search(arr) -> int:
 	:param arr: Array containing numbers
 	:return: index of first occurrence of minimal element in array
 	"""
+	index1 = 0
+	for i in range(1, len(arr)):
+		if arr[index1] > arr[i]:
+			index1 = i
+	return index1
 
-	return -1
 
 
 def min_weight_search(Graph) -> tuple:
@@ -21,5 +26,7 @@ def min_weight_search(Graph) -> tuple:
 	:param Graph: NetworkX Graph (or digraph) with weighted edges
 	:return: tuple of nodes (node, node) the weight of edge between which is minimal (any occurrence)
 	"""
-
 	return None, None
+
+
+print(min_search(array))
